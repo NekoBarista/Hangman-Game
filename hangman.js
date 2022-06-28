6e96f4ba5c8b2f4095e29003c0f7f2697c337151
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-06-28 18:56:17 +1000

    initial method added

--- hangman.js
+++ hangman.js
@@ -1,10 +1,33 @@
-const Hangman = function(word, guesses) {
-    this.word = word
+const Hangman = function(word, guesses, lettersGuessed) {
+    this.word = word.toLowerCase().split("")
     this.guesses = guesses
+    this.lettersGuessed = "h, e" }
+
+Hangman.prototype.getPuzzle = function (guess) {
+   let puzzle = ""
+this.word.forEach((letter) => 
+{
+if (this.lettersGuessed.includes(letter) || letter === "" )
+{
+puzzle += letter
 }
+
+else {
+   puzzle += "*" 
+}
+
+
+
+})
+   return puzzle
+
+    
+}
+
 
 let game1 = new Hangman('hello', 3)
 let game2 = new Hangman ('dance', 2)
 
-console.log(game1)
-console.log(game2)
+console.log(game1.getPuzzle("h e"))
+console.log(game2.getPuzzle("d a"))
+
