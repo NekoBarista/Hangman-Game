fe19097cfc4e79f888306d9bf734e5f37f19477a
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-06-29 21:04:55 +1000

    functionality wired up

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
-let game1 = new Hangman('car parts', 3)
+let game1 
+
+
 const puzzleEl = document.querySelector("#puzzle")
 const guessesEl = document.querySelector("#remaining")
 
-puzzleEl.textContent = game1.getPuzzle
-guessesEl.textContent =  game1.gameMessage
 
-
-window.addEventListener('keypress', function (e) {
+window.addEventListener('keypress', (e) => {
   const guess = e.key
   game1.makeGuess(guess)
   puzzleEl.textContent = game1.getPuzzle
 guessesEl.textContent =  game1.gameMessage
-
- 
-
+render()
 })
+
+
+const render = () => {
+  puzzleEl.textContent = game1.getPuzzle
+guessesEl.textContent =  game1.gameMessage
+
+}
+
+const newGame = async () => {
+  const puzzle = await createPuzzle()
+  game1 = new Hangman(puzzle, 5) 
+  render()
+
+}
+
+newGame()
+
+document.querySelector('#reset').addEventListener('click', newGame)
+
+
+
+
 
